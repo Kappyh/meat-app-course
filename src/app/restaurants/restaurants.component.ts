9315cc1d44caebe73f9bc563d7916f54b682c37c
Gabriela Mendes <gaperi_8@hotmail.com>
2018-03-18 15:36:10 -0300

    módulo de busca curso angular e operadores debounceTime, catch e distinctUtilChanged do rxjs

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,21 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/from';
+import { Observable } from 'rxjs/Observable';
+
 import { Restaurant } from 'app/restaurants/restaurant/restaurant.model';
 import { RestaurantsService } from 'app/restaurants/restaurants.service';
 
 @Component({
   selector: 'mt-restaurants',
-  templateUrl: './restaurants.component.html'
+  templateUrl: './restaurants.component.html',
+  animations: [
+    trigger('toggleSearch', [
+      state('hidden', style({
+        opacity: 0,
+        "max-height": "0px"
+      })),
+      state('visible', style({
+        opacity: 1,
+        "max-height": "70px",
+        "margin-top": "20px"
+      })),
+      transition('*=>*', animate('250ms 0s ease-in-out'))
+    ])
+  ]
 })
 export class RestaurantsComponent implements OnInit {
 
+  public searchBarState = 'hidden';
   public restaurants: Restaurant[] = undefined;
+  public searchForm: FormGroup;
+  public searchControl: FormControl;
 
-  constructor(private restaurantsServices: RestaurantsService) { }
+  constructor(private restaurantsServices: RestaurantsService,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
+
+    this.searchControl = this.fb.control('');
+    this.searchForm = this.fb.group({
+      searchControl: this.searchControl
+    });
+
     this.restaurantsServices.restaurants().subscribe(
       restaurants => this.restaurants = restaurants
     );
+
+    this.searchControl.valueChanges.debounceTime(500)
+      .distinctUntilChanged()
+      .switchMap(
+        searchTerm => this.restaurantsServices.restaurants(searchTerm)
+        .catch(error=>Observable.from([]))
+      ).subscribe(restaurants => this.restaurants = restaurants);
+  }
+
+  public toggleSearch() {
+    this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden';
   }
 
 }
